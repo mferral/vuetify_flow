9e917ed70d2d5d97d27842b9cf2a460439299b64
Eder <eder@gmail.com>
2019-08-30 01:19:31 -0500

    add started

--- src/views/beer2/Add.vue
+++ src/views/beer2/Add.vue
@@ -2,57 +2,42 @@
 <div>
   <v-container>
 
-
-
-
-
-
     <div class="mb-5 text-center">
       <h1 class="display-1">Cervezas</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit vel velit nisi suscipit, quae quas aspernatur illo rem sequi hic. Perferendis enim quis esse rerum quia quisquam soluta fuga debitis.</p>
       <hr>
     </div>
+    {{post}}
+    <v-form ref="form" v-model="valid" lazy-validation >
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="6">
+            <v-text-field v-model="post.name" label="Nombre" :rules="[v => !!v || 'Item is required']" required></v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field v-model="post.estilo"  label="Estilo" :rules="[v => !!v || 'Item is required']"  required></v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field v-model="post.color" label="Color" :rules="[v => !!v || 'Item is required']" required></v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field v-model="post.alcohol"  label="Alcohol" :rules="[v => !!v || 'Item is required']"  required></v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field v-model="post.pais" label="País" :rules="[v => !!v || 'Item is required']" required></v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field v-model="post.url"  label="URL" :rules="[v => !!v || 'Item is required']"  required></v-text-field>
+          </v-col>                    
 
 
-  <form>
-    <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="10"
-      label="Name"
-      required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :error-messages="emailErrors"
-      label="E-mail"
-      required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      :error-messages="selectErrors"
-      label="Item"
-      required
-      @change="$v.select.$touch()"
-      @blur="$v.select.$touch()"
-    ></v-select>
-    <v-checkbox
-      v-model="checkbox"
-      :error-messages="checkboxErrors"
-      label="Do you agree?"
-      required
-      @change="$v.checkbox.$touch()"
-      @blur="$v.checkbox.$touch()"
-    ></v-checkbox>
+        </v-row>
+        <v-btn color="success" class="mr-4" :loading="posts.loading" :disabled="!valid || posts.loading" @click.prevent="guardar">
+          Guardar              
+        </v-btn>
+      </v-container>
+    </v-form>
 
-    <v-btn class="mr-4" @click="submit">submit</v-btn>
-    <v-btn @click="clear">clear</v-btn>
-  </form>
 
 
 
@@ -64,7 +49,26 @@
 </template>
 
 <script>
-export default {
+  import {
+    mapState,
+    mapActions
+  } from 'vuex'
 
-}
+  export default {
+    data: () => ({
+      valid: true,
+      post: {}
+    }),
+    computed: {
+      ...mapState(["posts"])
+    },
+    methods: {
+      ...mapActions(["ADD_POST"]),
+      guardar(){
+        if (this.$refs.form.validate()) {                   
+          this.ADD_POST(this.post);
+        }
+      }
+    }
+  }  
 </script>
